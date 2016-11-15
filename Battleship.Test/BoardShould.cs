9711d2c5c9a8f2efc6cf11abf730ab723c4b67b7
Brianna Liston <listonbk@mail.uc.edu>
2016-11-15 13:21:43 -0500

    skeleton

--- Battleship.Test/BoardShould.cs
+++ Battleship.Test/BoardShould.cs
@@ -13,67 +13,79 @@ namespace Battleship.Test
             Board test = new Board();
         }
 
+        [TestMethod]
         public void CreateEmptyCell()
         {
 
         }
 
+        [TestMethod]
         public void ReadMissOnEmptyBoard()
         {
 
         }
 
+        [TestMethod]
         public void PlaceValidShip()
         {
 
         }
 
+        [TestMethod]
         public void ThrowIfStartedWithoutFullTeam()
         {
 
         }
 
-
+        [TestMethod]
         public void ShowAHit()
         {
 
         }
 
+        [TestMethod]
         public void ShowAMiss()
         {
 
         }
 
+        [TestMethod]
         public void ThrowOnInvalidPlacement()
         {
 
         }
 
+        [TestMethod]
         public void ThrowOnRedundantShot()
         {
 
         }
 
+        [TestMethod]
         public void HideOpponentShipsAppropriately()
         {
 
         }
 
+        [TestMethod]
         public void GiveWinWhenAllShipsDestroyed()
         {
 
         }
 
+        [TestMethod]
         public void DestroyFullyHitShip()
         {
 
         }
 
+        [TestMethod]
         public void SaveGameCorrectly()
         {
 
         }
 
+        [TestMethod]
         public void LoadGameCorrectly()
         {
 
