11e3e0318fea74809fbc993c783edd2283e516b5
Jacob Colvin <colvinjm@mail.uc.edu>
2016-11-15 13:00:53 -0500

    Added Tests

---
+++ Battleship.Test/BoardShould.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Battleship;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Battleship.Test
+{
+    [TestClass]
+    public class BoardShould
+    {
+        [TestMethod]
+        public void CreateEmptyBoard()
+        {
+            Board test = new Board();
+        }
+    }
+}
