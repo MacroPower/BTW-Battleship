a25c5de482438ac9e2fca41bfd9a28a2fcf54e96
Jacob Colvin <colvinjm@mail.uc.edu>
2016-11-15 13:41:48 -0500

    Trying a few things.

--- Battleship/Board.cs
+++ Battleship/Board.cs
@@ -8,9 +8,26 @@ namespace Battleship
 {
     public class Board
     {
+        private int[,] array = new int[10, 10];
 
         public Board()
         {
+            //for col
+            for (int i = 1; i <= 10; i++)
+            {
+               
+                array[col, i] = 0;
+            } // 0 = no info
+        }
+
+        public int GetCellStatus(int x, int y)
+        {
+            return array[x,y];
+        }
+
+        public void Shot ()
+        {
+            //need to set shots?
         }
     }
 }
