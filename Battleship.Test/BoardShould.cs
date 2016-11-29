6ebd11600b6efc25f295d9d7a52cf50f1ce723bd
Brianna Liston <listonbk@mail.uc.edu>
2016-11-29 13:13:58 -0500

    slightly better syntax

--- Battleship.Test/BoardShould.cs
+++ Battleship.Test/BoardShould.cs
@@ -14,15 +14,10 @@ namespace Battleship.Test
         }
 
         [TestMethod]
-        public void CreateEmptyCell()
-        {
-            Cell test = new Cell();
-        }
-
-        [TestMethod]
         public void ReadMissOnEmptyBoard()
         {
-            Board.Shot(6,6);
+            Board test = new Board();
+            test.Shot(6,6);
             Assert.AreEqual(false);
         }
 
@@ -47,15 +42,17 @@ namespace Battleship.Test
         public void ShowAHit()
         {
             Ship test1 = new Ship(4, 2, 2, 6, 2);
-            Board.Shot(2,2);
+            Board test = new Board();
+            test.Shot(2,2);
             Assert.AreEqual(true);
         }
         //show miss
         [TestMethod]
         public void ShowAMiss()
         {
+            Board test = new Board();
             Ship test1 = new Ship(4, 2, 2, 6, 2);
-            Board.Shot(6, 6);
+            test.Shot(6, 6);
             Assert.AreEqual(false);
         }
 
@@ -63,46 +60,50 @@ namespace Battleship.Test
         [ExpectedException(typeof(Exception), "Not a valid ship placement.")]
         public void ThrowOnInvalidPlacement()
         {
-            Board.Shot(6, 11);
+            Board test = new Board();
+            test.Shot(6, 11);
         }
 
         [TestMethod]
         [ExpectedException(typeof(Exception), "Already fired at these coordinates.")]
         public void ThrowOnRedundantShot()
         {
-            Board.Shot(6, 6);
-            Board.Shot(6, 6);
+            Board test = new Board();
+            test.Shot(6, 6);
+            test.Shot(6, 6);
         }
 
         [TestMethod]
         public void GiveWinWhenAllShipsDestroyed()
         {
+            Board test = new Board();
+
             Ship test1 = new Ship(4, 2, 2, 5, 2);
-            Board.Shot(2, 2);
-            Board.Shot(3, 2);
-            Board.Shot(4, 2);
-            Board.Shot(5, 2);
+            test.Shot(2, 2);
+            test.Shot(3, 2);
+            test.Shot(4, 2);
+            test.Shot(5, 2);
 
-            Ship test1 = new Ship(3, 2, 3, 4, 3);
-            Board.Shot(2, 3);
-            Board.Shot(3, 3);
-            Board.Shot(4, 3);
+            Ship test2 = new Ship(3, 2, 3, 4, 3);
+            test.Shot(2, 3);
+            test.Shot(3, 3);
+            test.Shot(4, 3);
 
-            Ship test1 = new Ship(3, 2, 4, 4, 4);
-            Board.Shot(2, 4);
-            Board.Shot(3, 4);
-            Board.Shot(4, 4);
+            Ship test3 = new Ship(3, 2, 4, 4, 4);
+            test.Shot(2, 4);
+            test.Shot(3, 4);
+            test.Shot(4, 4);
 
-            Ship test1 = new Ship(2, 9, 10, 10, 10);
-            Board.Shot(9, 10);
-            Board.Shot(10, 10);
+            Ship test4 = new Ship(2, 9, 10, 10, 10);
+            test.Shot(9, 10);
+            test.Shot(10, 10);
 
-            Ship test1 = new Ship(5, 2, 6, 6, 6);
-            Board.Shot(2, 6);
-            Board.Shot(3, 6);
-            Board.Shot(4, 6);
-            Board.Shot(5, 6);
-            Board.Shot(6, 6);
+            Ship test5 = new Ship(5, 2, 6, 6, 6);
+            test.Shot(2, 6);
+            test.Shot(3, 6);
+            test.Shot(4, 6);
+            test.Shot(5, 6);
+            test.Shot(6, 6);
 
             Assert.AreEqual(true);
         }
@@ -110,11 +111,12 @@ namespace Battleship.Test
         [TestMethod]
         public void DestroyFullyHitShip()
         {
+            Board test = new Board();
             Ship test1 = new Ship(4, 2, 2, 6, 2);
-            Board.Shot(2, 2);
-            Board.Shot(3, 2);
-            Board.Shot(4, 2);
-            Board.Shot(5, 2);
+            test.Shot(2, 2);
+            test.Shot(3, 2);
+            test.Shot(4, 2);
+            test.Shot(5, 2);
             Assert.AreEqual(true);
         }
     }
